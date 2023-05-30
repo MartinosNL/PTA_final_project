d421882b41828c28007aaef1564e298299a55030
C <c.van.der.deen@student.rug.nl>
2023-05-30 17:05:43 +0200

    nltk chunk addition, outlines for potential other systems

--- classify_entities.py
+++ classify_entities.py
@@ -5,7 +5,27 @@ from nltk import ne_chunk
 import spacy
 
 
-def tag_entities(filepath, NER):
+def gpe_disambig(string):
+    '''This will handle the separation between COU and CIT.'''
+    return "dummy"
+
+
+def fit_ner(string):
+    '''This will handle turning the chunk labels into the desired labels.'''
+    if string == "PERSON":
+        return "PER"
+    elif string == "ORGANIZATION":
+        return "ORG"
+    elif string == "GPE" or string == "GSP":
+        return gpe_disambig(string)
+    else:
+        return None
+    # TODO: No idea how to find animals, sports, nature and entertainment.
+    # Will do research into this but will likely have to be done with a separate
+    # tagging system through sysnet.
+
+
+def tag_entities(filepath):
     # Open file and put contents in string.
     str_file_contents = ""
     with open(filepath, 'r') as file_input:
@@ -13,31 +33,40 @@ def tag_entities(filepath, NER):
             str_file_contents += line
 
     # Create NER tags.
-    dict_ner = {}
-    list_sent = []
+    list_pos = []
     for line in str_file_contents.split("\n"):
         current_split = line.split(" ")
         if len(current_split) >= 4:
-            list_sent.append(current_split[3])
-    current_sent = " ".join(list_sent)
-    # Stopping here for now. TODO: Adhere to the tags demanded
-    # by the assignment and format the NEs in a way appendable in the code
-    # underneath here.
+            tuple_current = (current_split[3], current_split[4])
+            list_pos.append(tuple_current)
+    ne_tags = ne_chunk(list_pos)
+    
+    # Clean up NER tags.
+    list_tags = []
+    for ne in ne_tags:
+        if hasattr(ne, 'label'):
+            for i in range(len(ne)): # We do this to handle multi-word entities.
+                list_tags.append(ne.label())
+        else:
+            list_tags.append(None)
 
     # Add tag to lines in string where applicable.
     list_tagged = []
     accum_line = 0
     for line in str_file_contents.split("\n"):
         if line != "":
-            line += " "
-            line += "dummy"
-            #print(line)
+            if list_tags[accum_line] != None:
+                made_tag = fit_ner(list_tags[accum_line])
+                if made_tag != None:
+                    line += " "
+                    line += made_tag
+            # Sysnet functionality per word could be added here.
             list_tagged.append(line)
             accum_line += 1
     return "\n".join(list_tagged)
 
 def create_filepaths(path_input):
-    '''Returns all filepaths leading to "en.tok.off.pos" files.'''
+    '''Returns a list of filepaths leading to "en.tok.off.pos" files.'''
     list_append = []
     list_return = []
     for item in path_input.iterdir():
@@ -56,11 +85,10 @@ def create_filepaths(path_input):
 
 
 def main():
-    NER = spacy.load("en_core_web_sm")
     path_base = pathlib.Path(sys.argv[1])
     list_paths = create_filepaths(path_base)
     for filepath in list_paths:
-        tag_entities(filepath, NER)
+        print(tag_entities(filepath))
 
 
 main()
